a314a5785d0a60870f6c5639aad9dfc6c82ba7b7
varun visakamalla <98416039+visakamallavarun@users.noreply.github.com>
2024-10-31 15:02:18 +0530

    Small changes

--- projects/image-selector-lib/src/public-api.ts
+++ projects/image-selector-lib/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/image-selector-lib.component';
 export * from './lib/image-selector-lib.module';
 export * from './lib/components/image-selector/image-selector.component';
 export * from './lib/components/image-selector/image.service';
+export * from './lib/config';
